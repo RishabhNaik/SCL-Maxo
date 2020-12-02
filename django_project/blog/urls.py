fec91c4ee7938c4132741f68063e77097dd4caac
RishabhNaik <rishabhnaik37@gmail.com>
2020-12-02 16:31:51 +0530

    Added all the files for development

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home,name='blog-home' ),
     path('about/', views.about,name='blog-about')
+    path('register/', views.about,name='blog-register')
+    path('login/', views.about,name='blog-login')
 ]
