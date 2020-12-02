9a8413cfa739f4d31e42197468048ee2debb45ef
RishabhNaik <rishabhnaik37@gmail.com>
2020-12-02 09:30:11 +0530

    Added all the files for development

---
+++ django_project/blog/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+# Create your views here.
+posts= [
+    {
+        'author':'Rishabh',
+        'title':'blog-post1',
+        'content':'First post',
+        'date_posted':'August 27, 2018'
+    },
+    {
+        'author':'Hrishi',
+        'title':'blog-post2',
+        'content':'Second post',
+        'date_posted':'August 29, 2018'
+    }
+
+]
+
+
+def home(request):
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html',context)
+
+def about(request):
+    return render(request, 'blog/about.html',{'title':'About'})
+
+def register(request):
+    return render(request, 'blog/about.html',{'title':'Register'})
+
+def login(request):
+    return render(request, 'blog/login.html',{'title':'Login'})
+
+
